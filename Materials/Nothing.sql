ab53a41901f04bf00b49550f1fd364200cfe75c7
Roman <164523311+Korablinr22@users.noreply.github.com>
2024-06-16 19:15:13 +0700

    Update Nothing.sql

--- Materials/Nothing.sql
+++ Materials/Nothing.sql
@@ -1,2 +1,7 @@
-SELECT *
-  FROM Everything
+SELECT
+members.firstname AS "First Name",
+members.lastname AS "Last Name"
+FROM borrowings
+JOIN books ON borrowings.bookid=books.bookid
+JOIN members ON members.memberid=borrowings.memberid
+WHERE books.author='Dan Brown';
